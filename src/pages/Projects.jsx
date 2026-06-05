868881776ff2b0fc66d6f380aab3c79d57ba0ee4
Naren <ktmnaren@gmail.com>
2026-06-05 22:32:13 +0530

    Mobile number corrections

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -253,7 +253,7 @@ export default function Projects() {
                         : "text-sm"
                     }`}
                   >
-                    📞 +91 9585275171
+                    📞 +91 95852 75171
                   </p>
 
                   <p
@@ -263,7 +263,7 @@ export default function Projects() {
                         : "text-sm"
                     }`}
                   >
-                    📞 +91 8675783639
+                    📞 +91 86757 83639
                   </p>
 
                   <p
@@ -279,7 +279,7 @@ export default function Projects() {
                   <p
                     className={`text-gray-300 ${
                       i18n.language === "ta"
-                        ? "text-[14px] leading-[1.8]"
+                        ? "text-[14px] leading-[1.7]"
                         : "text-sm"
                     }`}
                   >
--- src/sections/FreeConsultation.jsx
+++ src/sections/FreeConsultation.jsx
@@ -108,7 +108,7 @@ export default function FreeConsultation() {
                 data-aos-delay="300"
               >
                 <FaPhoneAlt className="text-[oklch(0.47_0.17_28.33)]" />
-                +91 9585275171
+                +91 95852 75171
               </p>
 
               <p
@@ -117,7 +117,7 @@ export default function FreeConsultation() {
                 data-aos-delay="300"
               >
                 <FaPhoneAlt className="text-[oklch(0.47_0.17_28.33)]" />
-                +91 8675783639
+                +91 86757 83639
               </p>
 
               <p
